e22b269b7c556501ac5b8c4a7a9ce1945c696c41
Kevin Bravo <kbravo1024@gmail.com>
2023-07-01 11:12:42 -0700

    added comments

--- src/components/UserMonth.js
+++ src/components/UserMonth.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 function UserMonth({ sub, filter }) {
+  //turn number into currency
   let totalInDollars = new Intl.NumberFormat(`en-US`, {
     style: "currency",
     currency: "USD",
   });
+
+  //adds all prices and sets sum to total
   let sum = 0;
   sub.forEach((element) => {
     sum += element.price;
   });
 
+  //gets substring and returns filter
   function newFilter() {
     const newText = filter.split("=")[1].toUpperCase();
     return newText;
